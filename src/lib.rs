0fb146ce26bdea225acae8db70a7227b546cea04
Cameron Raw <cameron.raw89@gmail.com>
2023-03-05 18:40:07 +0100

    Adds first integration test - for health check endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, dev::Server};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
-            .route("/health-check", web::get().to(health_check))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    .run();
+
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use zero_to_production::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    run()?.await
 }
