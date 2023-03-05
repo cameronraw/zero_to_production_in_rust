bdd265328313c1391a0322997cf59a6418790488
Cameron Raw <cameron.raw89@gmail.com>
2023-03-05 19:02:56 +0100

    Refactors integration tests to use random available port each run

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::net::TcpListener;
 use zero_to_production::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("172.0.0.1:8000");
+    match listener {
+        Ok(valid_listener) => run(valid_listener)?.await,
+        Err(err) => panic!("{}", err)
+    }
 }
