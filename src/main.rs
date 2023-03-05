89fa37a97bd708e5ab61b61a12473e8f5a1d0ad7
Cameron Raw <cameron.raw89@gmail.com>
2023-03-05 21:44:59 +0100

    Adds configuration capability

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use zero_to_production::configuration::get_configuration;
 use zero_to_production::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("172.0.0.1:8000");
-    match listener {
-        Ok(valid_listener) => run(valid_listener)?.await,
-        Err(err) => panic!("{}", err)
-    }
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
