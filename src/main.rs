98443362f49357dd483048ffacfc3dd2ebb751cb
Cameron Raw <cameron.raw89@gmail.com>
2023-03-06 01:03:22 +0100

    Introduces connection pooling and isolated db for each integration test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
-use sqlx::{PgConnection, Connection};
 use zero_to_production::configuration::get_configuration;
 use zero_to_production::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection = PgConnection::connect(
-        &configuration.database.connection_string()
-    )
-    .await
-    .expect("Failed to connect to Postgres");
+
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
